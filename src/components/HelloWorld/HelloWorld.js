7072a11b29ffa23479c044632408bc342a36e476
Blake Rollins <brollins90@gmail.com>
2017-07-12 18:45:13 -0600

    first example

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+/** A super lame component that says Hello with a custom message. */
 function HelloWorld({ message }) {
     return <div>Hello {message}</div>;
 }
 
 HelloWorld.propTypes = {
+    /** Message to display */
     message: PropTypes.string,
+};
+
+HelloWorld.defaultProps = {
+    message: 'World',
 };
 
 export default HelloWorld;
