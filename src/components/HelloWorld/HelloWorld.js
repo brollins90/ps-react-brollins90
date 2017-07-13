8d4eaf270f5efdfa6f2d12fef11140ef80252ad0
Blake Rollins <brollins90@gmail.com>
2017-07-12 18:17:39 -0600

    hello world component

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function HelloWorld({ message }) {
+    return <div>Hello {message}</div>;
+}
+
+HelloWorld.propTypes = {
+    message: PropTypes.string,
+};
+
+export default HelloWorld;
