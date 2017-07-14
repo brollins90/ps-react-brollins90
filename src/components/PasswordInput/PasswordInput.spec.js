3cc973d6c9d9f3b5a209ede6b65c56b17de425e7
Blake Rollins <brollins90@gmail.com>
2017-07-14 09:40:19 -0600

    interaction test

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import PasswordInput from './PasswordInput';
+import { shallow } from 'enzyme';
+
+test('toggles input type when show/hide password clicked', () => {
+    const wrapper = shallow(<PasswordInput
+        htmlId="test"
+        name="test"
+        value=""
+        onChange={() => { }}
+        showVisibilityToggle
+    />);
+
+    // Password input should have type password initially
+    expect(wrapper.find({ type: 'password' })).toHaveLength(1);
+    expect(wrapper.find({ type: 'text' })).toHaveLength(0);
+
+    wrapper.find('a').simulate('click');
+
+    // Password input should have type text after click
+    expect(wrapper.find({ type: 'password' })).toHaveLength(0);
+    expect(wrapper.find({ type: 'text' })).toHaveLength(1);
+});
 
 test('hides password quality by default', () => {
     const tree = renderer.create(<PasswordInput
@@ -8,5 +29,25 @@ test('hides password quality by default', () => {
         name="test"
         onChange={() => { }}
         value="Uisi38#8iad" />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('shows password quality when enabled and a password is entered', () => {
+    const tree = renderer.create(<PasswordInput
+        htmlId="test"
+        name="test"
+        onChange={() => { }}
+        showQuality
+        value="Uisi38#8iad" />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('hides password quality when enabled but no password is entered', () => {
+    const tree = renderer.create(<PasswordInput
+        htmlId="test"
+        name="test"
+        onChange={() => { }}
+        showQuality
+        value="" />).toJSON();
     expect(tree).toMatchSnapshot();
 });
