20a336a303fc02ce677aa288f94d4c25dccd602c
Blake Rollins <brollins90@gmail.com>
2017-07-17 19:03:43 -0600

    add index

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as EyeIcon } from './EyeIcon';
+export { default as Label } from './Label';
+export { default as PasswordInput } from './PasswordInput';
+export { default as ProgressBar } from './ProgressBar';
+export { default as RegistrationForm } from './RegistrationForm';
+export { default as TextInput } from './TextInput';
